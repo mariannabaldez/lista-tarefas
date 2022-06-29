3aaf66732aeb31da1131255bf78106baa88f9cf0
mariannabaldez <marianna.baldez@gmail.com>
2022-06-29 14:35:30 -0300

    feita funcão que adiciona tarefa

--- assets/script.js
+++ assets/script.js
@@ -2,13 +2,17 @@ const inputNovaTarefa = document.querySelector('.task-input');
 const listaTarefas = document.querySelector('.tasks-list');
 const buttonAdicionaTarefa = document.querySelector('.add-button');
 
+function gerarId() {
+	return Math.floor(Math.random() *3000);
+}
+
 inputNovaTarefa.addEventListener('keypress', (e) => {
 	if(e.keyCode == 13) { //keyCode corresponde a uma tecla do teclado. 13 corresponde a tecla ENTER
 		let tarefa = {
 			nome: inputNovaTarefa.value,
 			id: gerarId(),
 		}
-		//add tarefa ao html
+		adicionarTarefa(tarefa);
 	}
 })
 
@@ -17,18 +21,36 @@ buttonAdicionaTarefa.addEventListener('click', (e) => {
 		nome: inputNovaTarefa.value,
 		id: gerarId(),
 	}
-	//add tarefa ao html
+	adicionarTarefa(tarefa);
 })
-
-function geraId() {
-	return Math.floor(Math.random() *3000);
-}
 
 function adicionarTarefa(tarefa) {
 	let li = criarLi(tarefa);
 	listaTarefas.appendChild(li);
+	inputNovaTarefa.value = '';
 }
 
 function criarLi(tarefa) {
 	let li = document.createElement('li');
+
+	let span = document.createElement('span');
+	span.classList.add('tasks');
+	span.innerHTML = tarefa.nome;
+
+	let div = document.createElement('div');
+
+	let buttonEdit = document.createElement('button');
+	buttonEdit.classList.add('action-button');
+	buttonEdit.innerHTML = '<i class="fa fa-pencil"></i>';
+
+	let buttonDelete = document.createElement('button');
+	buttonDelete.classList.add('action-button');
+	buttonDelete.innerHTML =  '<i class="fa fa-trash"></i>';
+
+	div.appendChild(buttonEdit);
+	div.appendChild(buttonDelete);
+
+	li.appendChild(span);
+	li.appendChild(div);
+	return li;
 }
